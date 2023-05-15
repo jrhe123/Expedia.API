82a9dc4ca67351c283c35c41e06d3618ea78c550
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-15 00:16:53 -0400

    setup project

---
+++ Expedia.API/Program.cs
@@ -0,0 +1,33 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// register services into IoC container
+// to use api, we need mvc controller
+builder.Services.AddControllers();
+
+//
+var app = builder.Build();
+
+// environment variables
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+
+// http request pipeline (middleware)
+// e.g., logging <-> static file <-> mvc
+// e.g., UseRouting <-> UseEndpoints
+// deprecated version
+app.UseRouting();
+app.UseEndpoints(endPoints =>
+{
+    endPoints.MapGet("/test", async context =>
+    {
+        await context.Response.WriteAsync("this is test api 123");
+    });
+});
+
+
+
+app.MapGet("/", () => "Hello World!");
+app.Run();
---
+++ notes.md
@@ -0,0 +1,8 @@
+﻿### DotNet Core 7.0
+
+- https://learn.microsoft.com/en-us/aspnet/core/fundamentals/environments?view=aspnetcore-7.0
+
+
+### Configuration (env variables / ports / dev server)
+
+- Properties/launchSettings.json
