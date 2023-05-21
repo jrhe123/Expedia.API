dbce8cf187b3dcfc994d55132a20d7adc7d81360
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-20 22:11:34 -0400

    add tourist route picture in create tourist route api

--- Expedia.API/Dtos/TouristRouteForCreatingDto.cs
+++ Expedia.API/Dtos/TouristRouteForCreatingDto.cs
@@ -19,6 +19,8 @@ namespace Expedia.API.Dtos
         public TravelDays? TravelDays { get; set; }
         public TripType? TripType { get; set; }
         public DepartureCity DepartureCity { get; set; }
+        public ICollection<TouristRoutePictureForCreatingDto> TouristRoutePictures { get; set; }
+            = new List<TouristRoutePictureForCreatingDto>();
     }
 }
 
