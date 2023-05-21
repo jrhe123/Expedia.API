81b8892285d5eb28678be6d9c108d1e36e8c263b
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-21 12:12:32 -0400

    add array model binder class

---
+++ Expedia.API/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+/**
+ * https://blog.csdn.net/weixin_43542114/article/details/115837088
+ */
+namespace Expedia.API.Helpers
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            var value = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName)
+                .ToString();
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+
+            var elementType = bindingContext.ModelType.GetTypeInfo()
+                .GenericTypeArguments.FirstOrDefault();
+            var converter = TypeDescriptor.GetConverter(elementType);
+
+            var values = value.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => converter.ConvertFromString(x.Trim()))
+                .ToArray();
+
+            var typedValues = Array.CreateInstance(elementType, values.Length);
+            values.CopyTo(typedValues, 0);
+            bindingContext.Model = typedValues;
+
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
+
